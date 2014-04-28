a922aafca966b05c3862e236e7c711ec3a74612a
Rex Schrader <rex.schrader@gmail.com>
2014-04-28 21:04:48 +0000

    Adding invoice_fix.blade.php

---
+++ app/views/wp_fixes/invoice_fix.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.scaffold')
+
+@section('main')
+<h1>Invoice Fixing</h1>
+<table class="table table-striped table-bordered">
+	<thead>
+		<tr>
+			<th>Invoice No</th>
+			<th>Username</th>
+			<th>School_id</th>
+			<th>Empty?</th>
+			<th>User School ID</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($invoices as $invoice)
+		<tr>
+			<td>{{ $invoice->invoice_no }}</td>
+			@if($invoice->user->usermeta->isEmpty())
+				<td>{{$invoice->user->metadata['fullname'] }}</td>
+			@else
+				<td>{{ $invoice->user->display_name }}</td>
+			@endif
+			<td>{{ $invoice->school_id }}</td>
+			<td>{{ $invoice->user->usermeta->isEmpty() ? "Empty" : "Not Empty" }}</td>
+			@if($invoice->user->usermeta->isEmpty())
+				<td>No Value</td>
+			@else
+				<td>{{ $invoice->user->usermeta->first()->meta_value }}</td>
+			@endif
+		</tr>
+		@endforeach
+	</tbody>
+</table>
+
+@stop
