9315f0005c47240e2f7b0f31310b8ac164da1e71
SaintPeter <rex.schrader@gmail.com>
2014-05-14 21:48:43 -0700

    Fix Paths

--- app/views/partials/filelist.blade.php
+++ app/views/partials/filelist.blade.php
@@ -6,12 +6,12 @@
 				@if($show_type) <td>{{ $file->filetype->name }}</td> @endif
 				@if($file->filetype->type == 'code' OR $file->filetype->type == 'img' OR ($file->filetype->type == 'doc' and ($file->filetype->ext == 'txt' or $file->filetype->ext == 'pdf')))
 					<td>{{ link_to($file->url(), $file->filename, [ 'target' => '_blank', 'class' => 'lytebox', 'data-title' => $file->filename, "data-lyte-options" => "group:group1" ]) }}</td>
-					<td><a href="{{ $file->path() }}"><span class="glyphicon glyphicon-download" title="Download File"></span></a></td>
+					<td><a href="{{ url($file->path()) }}" target="_blank"><span class="glyphicon glyphicon-download" title="Download File"></span></a></td>
 				@else
 					<td>{{ link_to($file->path(), $file->filename, [ 'target' => '_blank' ]) }}</td>
 					<td>&nbsp;</td>
 				@endif
-				@if($show_delete) 
+				@if($show_delete)
 					<td><a href="{{ route('uploader.delete_file', [ 'video_id' => $video->id, 'file_id' => $file->id ]) }}"
 					><span class="glyphicon glyphicon-remove" style="color: red;" title="Delete File"></span> </a></td>
 				@endif
