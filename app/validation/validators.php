4313ad5fe610a483532ae242b8a47f50d9629d81
SaintPeter <rex.schrader@gmail.com>
2014-05-15 19:40:24 -0700

    Fixed YouTube Code Validation Logic

--- app/validation/validators.php
+++ app/validation/validators.php
@@ -2,23 +2,21 @@
 
 Validator::extend('yt_valid', function($attribute, $value, $parameters)
 {
-	if(preg_match("#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})#", $value)) return true;
-
-    return false;
+	return preg_match("#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})|(^[A-Za-z0-9_-]{5,11})#", $value);
 });
 
 Validator::extend('yt_embeddable', function($attribute, $value, $parameters)
 {
-	if(preg_match("#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})#", $value, $matches)) {
-		return yt_check($matches[2], 'embeddable');
+	if(preg_match("#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})|(^[A-Za-z0-9_-]{5,11})#", $value, $matches)) {
+		return yt_check(empty($matches[2]) ? $matches[3] : $matches[2], 'embeddable');
 	}
     return false;
 });
 
 Validator::extend('yt_public', function($attribute, $value, $parameters)
 {
-	if(preg_match("#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})#", $value, $matches)) {
-		return yt_check($matches[2], 'privacyStatus');
+	if(preg_match("#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})|(^[A-Za-z0-9_-]{5,11})#", $value, $matches)) {
+		return yt_check(empty($matches[2]) ? $matches[3] : $matches[2], 'privacyStatus');
 	}
     return false;
 });
@@ -29,6 +27,7 @@ function yt_check($code, $option) {
 	if(!isset($data)) {
 		$result = curlhelper("https://www.googleapis.com/youtube/v3/videos/?part=status&id=" . $code . "&alt=json&key=AIzaSyCSAgGi8ROocoKToii0j20BXv0HNcSqWOg");
 		$data = json_decode($result);
+		//dd($code, $data);
 	}
 	if(count($data->items) > 0) {
 		return $data->items[0]->status->$option;
@@ -38,6 +37,7 @@ function yt_check($code, $option) {
 
 function curlhelper($url) {
 	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
 	curl_setopt($curl, CURLOPT_URL, $url);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER ,true);
 	$response = curl_exec($curl);
