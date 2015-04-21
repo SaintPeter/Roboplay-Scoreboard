71dd74b907458d2771bac7c7fb8e814f57ad6e22
SaintPeter <rex.schrader@gmail.com>
2015-04-20 19:57:43 -0700

    Fixed table case issue

--- app/models/Video_comment.php
+++ app/models/Video_comment.php
@@ -2,7 +2,7 @@
 
 class Video_comment extends \Eloquent {
 	protected $guarded = [ 'id' ];
-	protected $table = 'Video_comment';
+	protected $table = 'video_comment';
 
 	public function video() {
 		return $this->belongsTo('Video');
