9755b4944f37a461400141452cec27ed4585d69f
Rex Schrader <rex.schrader@gmail.com>
2016-05-06 10:40:21 -0700

    Move to student dialog partials

--- app/views/teacher/teams/edit.blade.php
+++ app/views/teacher/teams/edit.blade.php
@@ -79,19 +79,7 @@
 	savedIndex = {{ $index + 1 }};
 </script>
 
-<div id="choose_students_dialog" title="Choose Students">
-
-</div>
-
-<div id="mass_upload_students_dialog" title="Choose Upload File">
-Download this <a href="/scoreboard/docs/roboplay_scoreboard_student_upload_template.xls">Excel Template</a> and follow the instructions inside to generate a csv file for upload.
-  {{ Form::open([ 'route' => 'ajax.import_students_csv', 'files' => true, 'id' => 'upload_form' ] ) }}
-	  {{ Form::label('csv_file', 'CSV File', array('id'=>'','class'=>'')) }}
-  	  {{ Form::file('csv_file') }}
-  <br/>
-  <!-- submit buttons -->
-  {{ Form::close() }}
-</div>
+@include('students.partial.dialogs')
 
 @if ($errors->any())
 <div class="col-md-6">
