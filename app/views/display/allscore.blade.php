f7a31e473331d9e43f8a1eff7187fa68706b47f4
SaintPeter <rex.schrader@gmail.com>
2016-05-07 08:38:09 -0700

    Move Settings Dialog to Partial

--- app/views/display/allscore.blade.php
+++ app/views/display/allscore.blade.php
@@ -191,34 +191,6 @@
 	</div>
 </div>
 
-<div id="dialog-settings" title="Adjust Settings">
-	{{ Form::open( [ 'route' => [ 'display.all_scores_settings', $compyear->id ], 'class' => 'form-horizontal', 'id' => 'settings_form', 'style' => 'margin: 5px;' ] ) }}
-		<div class="form-group">
-			{{ Form::label('columns', 'Columns:', [ 'class' => 'col-sm-4 control-label' ]) }}
-			<div class="col-sm-7">
-				{{ Form::text('columns', $settings['columns'] , [ 'class'=>'form-control' ]) }}
-			</div>
-		</div>
-		<div class="form-group">
-			{{ Form::label('rows', 'Rows:', [ 'class' => 'col-sm-4 control-label' ]) }}
-			<div class="col-sm-7">
-				{{ Form::text('rows', $settings['rows'] , [ 'class'=>'form-control' ]) }}
-			</div>
-		</div>
-		<div class="form-group">
-			{{ Form::label('delay', 'Delay (ms):', [ 'class' => 'col-sm-4 control-label' ]) }}
-			<div class="col-sm-7">
-				{{ Form::text('delay', $settings['delay'] , [ 'class'=>'form-control' ]) }}
-			</div>
-		</div>
-		<div class="form-group">
-			{{ Form::label('font-size', 'Font Size:', [ 'class' => 'col-sm-4 control-label' ]) }}
-			<div class="col-sm-7">
-				{{ Form::text('font-size', $settings['font-size'] , [ 'class'=>'form-control' ]) }}
-			</div>
-		</div>
-
-	{{ Form::close() }}
-</div>
+@include('display.partial.settings', [ 'route' => 'display.all_scores_settings', 'id' => $compyear->id ]);
 
 @stop
